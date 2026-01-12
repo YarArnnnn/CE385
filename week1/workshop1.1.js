b3c604062e4fe9248a76146decf54b97a7fc4d22
Wisarut <66111966@dpu.ac.th>
2026-01-12 14:10:07 +0700

    Update workshop1.1.js

--- week1/workshop1.1.js
+++ week1/workshop1.1.js
@@ -1,7 +1,7 @@
-let name = "สมตุ๋ย";
+let name = "สมตุ๋ย"; // เก็บชื่ออายุสาขา
 let age = 18;
 let saka = "วิศวกรรมคอมพิวเตอร์";
 
-console.log("ชื่อ" + name);
+console.log("ชื่อ" + name); // แสดงชื่อ อายุ สาขา
 console.log("อายุ" + age);
 console.log("สาขาวิชา" + saka);
